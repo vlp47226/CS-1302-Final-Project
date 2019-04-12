19bf75e8770797921b8e59a8395b4b5da556e6b6
Victor Philippe <vlp47226@uga.edu>
2019-04-12 13:46:19 -0400

    added more imports and added a text object to 2048

--- src/main/java/cs1302/arcade/TwentyFortyEight.java
+++ src/main/java/cs1302/arcade/TwentyFortyEight.java
@@ -3,25 +3,24 @@ package cs1302.arcade;
 //imports
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
 
 
 public class TwentyFortyEight extends Application {
 
 
+    Text gameName;
+
+    
 
     @Override
     public void start(Stage stage) {
-
-
-
-
-
-
-
-
-
-
-
+        gameName = new Text("2048");
+        gameName.setfont(Font.font("clear sans", 20));
+        gameName.setFill(Color.Black);
+        
 
 
 
