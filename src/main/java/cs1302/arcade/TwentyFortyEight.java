aad3f908971eabb099a21bd24ef104b86d4f3f24
Victor Philippe <vlp47226@uga.edu>
2019-04-12 13:09:27 -0400

    added the skeleton code for 2048

---
+++ src/main/java/cs1302/arcade/TwentyFortyEight.java
@@ -0,0 +1,52 @@
+package cs1302.arcade;
+
+//imports
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class TwentyFortyEight extends Application {
+
+
+
+    @Override
+    public void start(Stage stage) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }//start
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}//TwentyFortyEight
