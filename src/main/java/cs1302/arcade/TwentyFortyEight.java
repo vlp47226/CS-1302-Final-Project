b51b576616451104f6dd02e12a583f9f0e9b2475
Dillon Loubser <dcl11282@uga.edu>
2019-04-13 14:03:02 -0400

    Added skeleton code to TwentyFortyEight

--- src/main/java/cs1302/arcade/TwentyFortyEight.java
+++ src/main/java/cs1302/arcade/TwentyFortyEight.java
@@ -6,11 +6,15 @@ import javafx.stage.Stage;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.Font;
-
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
 
 public class TwentyFortyEight extends Application {
 
-
+    Group group;
     Text gameName;
 
     
@@ -20,14 +24,58 @@ public class TwentyFortyEight extends Application {
         gameName = new Text("2048");
         gameName.setFont(Font.font("clear sans", 20));
         gameName.setFill(Color.BLACK);
+        group = new Group();
         
+        group.setOnKeyPressed(createKeyHandler()); // left-right key presses move the rectangle
 
+        Scene scene = new Scene(group, 640, 480);
+        stage.setTitle("cs1302-arcade!");
+        stage.setScene(scene);
+        stage.sizeToScene();
+        stage.show();
 
 
 
     }//start
 
+    private EventHandler<? super KeyEvent> createKeyHandler() {
+        return event -> {
+            System.out.println(event);
+            if (event.getCode() == KeyCode.LEFT){
+                shiftToLeft();
+                System.out.println("LEFT");
+            }
+            if (event.getCode() == KeyCode.RIGHT){
+                shiftToRight();
+                System.out.println("RIGHT");
+            }
+            if (event.getCode() == KeyCode.UP){
+                shiftToUp();
+                System.out.println("UP");
+            }
+            if (event.getCode() == KeyCode.DOWN){
+                shiftToDown();
+                System.out.println("DOWN");
+            }
+            // TODO bounds checking
+        };
+    } // createKeyHandler
+    
+    public void shiftToRight(){
 
+    }
+
+    public void shiftToLeft(){
+
+    }
+
+    public void shiftToUp(){
+
+    }
+
+    public void shiftToDown(){
+
+    }
 
 
 
