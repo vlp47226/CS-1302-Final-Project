12942d4d76ef469a3e0188476c48b5a60b680735
Dillon Loubser <dcl11282@uga.edu>
2019-04-15 13:59:22 -0400

    Added code for showing the 2048 title and score.

--- src/main/java/cs1302/arcade/TwentyFortyEight.java
+++ src/main/java/cs1302/arcade/TwentyFortyEight.java
@@ -3,6 +3,8 @@ package cs1302.arcade;
 //imports
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.Font;
@@ -11,24 +13,42 @@ import javafx.scene.Scene;
 import javafx.event.EventHandler;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.KeyCode;
+import javafx.scene.control.Label;
+import javafx.scene.layout.TilePane;
 
 public class TwentyFortyEight extends Application {
 
-    Group group;
     Text gameName;
-
+    Label label;
+    public int score;
+    VBox vbox;
+    HBox hbox;
+    TilePane tilePane;
     
 
     @Override
     public void start(Stage stage) {
         gameName = new Text("2048");
-        gameName.setFont(Font.font("clear sans", 20));
+        gameName.setFont(Font.font("clear sans", 50));
         gameName.setFill(Color.BLACK);
-        group = new Group();
+        label = new Label("Score: 0");
+        score = 0;
+        vbox = new VBox();
+        hbox = new HBox();
+        tilePane = new TilePane(5, 5);
+        tilePane.setPrefColumns(4);
+        tilePane.setPrefRows(4);
+        for(int i = 0; i < 16; i++){
+            tilePane.getChildren().add(new VBox());
+        }
+        hbox.setSpacing(450.0);
+        hbox.getChildren().addAll(gameName, label);
+        tilePane.setOnKeyPressed(createKeyHandler()); // left-right key presses move the rectangle
         
-        group.setOnKeyPressed(createKeyHandler()); // left-right key presses move the rectangle
+        vbox.getChildren().addAll(hbox, tilePane);
+        
 
-        Scene scene = new Scene(group, 640, 480);
+        Scene scene = new Scene(vbox, 640, 480);
         stage.setTitle("cs1302-arcade!");
         stage.setScene(scene);
         stage.sizeToScene();
@@ -77,7 +97,11 @@ public class TwentyFortyEight extends Application {
 
     }
 
-
+    public void addToScore(int x){
+        score += x;
+        label = new Label("Score: " + score);
+        hbox.getChildren().set(1, label);
+    }
 
 
 }//TwentyFortyEight
